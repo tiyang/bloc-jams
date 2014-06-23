9940a372da1439fe7ebd80870f9fc72f153db85b
tiyang <loutiyang@gmail.com>
2014-06-23 08:58:17 -0500

    added a profile and subscription page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -258,6 +258,7 @@ if (document.URL.match(/\/album.html/)) {
 require("./landing");
 require('./collection');
 require('./album');
+require('./profile');
 
 });
 
@@ -355,6 +356,28 @@ $(document).ready(function(){
   $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
 
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+ var tabsContainer = ".user-profile-tabs-container";
+ var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+   selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+ 
+ if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 });
 
 ;
