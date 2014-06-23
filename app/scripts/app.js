144f2146822bffe16f394ebb7006854f6eb54d4e
tiyang <loutiyang@gmail.com>
2014-06-23 10:33:43 -0500

    converted landing page to angular, created shuffle album

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,33 @@
-require("./landing");
-require('./collection');
-require('./album');
-require('./profile');
+//require("./landing");
+//require('./collection');
+//require('./album');
+//require('./profile');
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.appName = "Bloc Jams";
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+
+  $scope.appNameClicked = function() {
+    function shuffle(o) {
+      for (var j, x, i = o.length; i; j = Math.floor(Math.random()*i), x=o[--i], o[i]=o[j],o[j]=x);
+        return o;
+    };
+    $scope.albumURLs = shuffle($scope.albumURLs);
+  };
+
+  $scope.albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg',
+  ];
+}]);
