427e48d6d773056bd14a5e644b19aac4a828b8b2
tiyang <loutiyang@gmail.com>
2014-06-27 22:38:39 -0500

    changed slider to update data on change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,11 +7,11 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
 
@@ -158,6 +158,12 @@ blocJams.service('SongPlayer', function() {
      this.setSong(this.currentAlbum, song);
     },
 
+    seek: function(time) {
+      if(currentSoundFile) {
+        currentSoundFile.setTime(time);
+      }
+    },
+
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -184,19 +190,46 @@ blocJams.directive('slider', ['$document', function($document) {
     offsetXPercent = Math.min(1, offsetXPercent);
     return offsetXPercent;
    };
+  
+  var numberFromValue = function(value, defaultValue) {
+    if (typeof value === 'number') {
+      return value;
+    }
+
+    if (typeof value === 'undefined') {
+      return defaultValue;
+    }
+
+    if (typeof value === 'string') {
+      return Number(value);
+    }
+  };
 
   return {
     templateUrl: '/templates/directives/slider.html',
     replace: true,
     restrict: 'E',
-    scope: {},
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
 
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+
+
       var percentString = function() {
-        percent = Number(scope.value) / Number(scope.max) * 100;
+        var value =scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + '%';
       };
 
@@ -211,6 +244,7 @@ blocJams.directive('slider', ['$document', function($document) {
       scope.onClickSlider = function(event) {
         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
         scope.value = percent * scope.max;
+        notifyCallback(scope.value);
       };
 
       scope.trackThumb = function() {
@@ -218,6 +252,7 @@ blocJams.directive('slider', ['$document', function($document) {
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.$apply(function(){
             scope.value = percent * scope.max;
+            notifyCallback(scope.value);
           });
         });
  
@@ -226,6 +261,12 @@ blocJams.directive('slider', ['$document', function($document) {
           $document.unbind('mousemove.thumb');
           $document.unbind('mouseup.thumb');
         });
+      };
+
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
       };
 
     }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -264,11 +264,11 @@ if (document.URL.match(/\/album.html/)) {
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
 
@@ -415,6 +415,12 @@ blocJams.service('SongPlayer', function() {
      this.setSong(this.currentAlbum, song);
     },
 
+    seek: function(time) {
+      if(currentSoundFile) {
+        currentSoundFile.setTime(time);
+      }
+    },
+
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -441,19 +447,46 @@ blocJams.directive('slider', ['$document', function($document) {
     offsetXPercent = Math.min(1, offsetXPercent);
     return offsetXPercent;
    };
+  
+  var numberFromValue = function(value, defaultValue) {
+    if (typeof value === 'number') {
+      return value;
+    }
+
+    if (typeof value === 'undefined') {
+      return defaultValue;
+    }
+
+    if (typeof value === 'string') {
+      return Number(value);
+    }
+  };
 
   return {
     templateUrl: '/templates/directives/slider.html',
     replace: true,
     restrict: 'E',
-    scope: {},
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
       var $seekBar = $(element);
 
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+
+
       var percentString = function() {
-        percent = Number(scope.value) / Number(scope.max) * 100;
+        var value =scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + '%';
       };
 
@@ -468,6 +501,7 @@ blocJams.directive('slider', ['$document', function($document) {
       scope.onClickSlider = function(event) {
         var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
         scope.value = percent * scope.max;
+        notifyCallback(scope.value);
       };
 
       scope.trackThumb = function() {
@@ -475,6 +509,7 @@ blocJams.directive('slider', ['$document', function($document) {
           var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
           scope.$apply(function(){
             scope.value = percent * scope.max;
+            notifyCallback(scope.value);
           });
         });
  
@@ -483,6 +518,12 @@ blocJams.directive('slider', ['$document', function($document) {
           $document.unbind('mousemove.thumb');
           $document.unbind('mouseup.thumb');
         });
+      };
+
+      var notifyCallback = function(newValue) {
+        if(typeof scope.onChange === 'function') {
+          scope.onChange({value: newValue});
+        }
       };
 
     }
